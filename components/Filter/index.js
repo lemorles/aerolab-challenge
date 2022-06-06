fc9082e6e2ec5ffa22f4939dfa2cf1d29ee15d61
Leandro Morales <leomoralesarce@gmail.com>
2022-06-06 11:43:52 -0300

    add Filter section

---
+++ components/Filter/index.js
@@ -0,0 +1,132 @@
+import styled from 'styled-components';
+
+export default function Filter({
+  filter, setFilter, count, setCurrentPage, currentPage, totalPages,
+}) {
+  const handleFilter = (filterText) => {
+    setFilter(filterText);
+    setCurrentPage(0);
+  };
+
+  return (
+    <FilterContainer>
+      <Container>
+        <ProductsNumber>
+          {count / (totalPages + 1)}
+          {' '}
+          of
+          {' '}
+          {count}
+          {' '}
+          products
+        </ProductsNumber>
+        <Divider />
+        <FilterTitle>
+          Sort by:
+        </FilterTitle>
+        <FilterList>
+          <FilterItem
+            onClick={() => handleFilter('Most recent')}
+            className={filter === 'Most recent' ? 'active' : ''}
+          >
+            <Button>Most recent</Button>
+          </FilterItem>
+          <FilterItem
+            onClick={() => handleFilter('Lowest price')}
+            className={filter === 'Lowest price' ? 'active' : ''}
+          >
+            <Button>Lowest price</Button>
+          </FilterItem>
+          <FilterItem
+            onClick={() => handleFilter('Highest price')}
+            className={filter === 'Highest price' ? 'active' : ''}
+          >
+            <Button>Highest price</Button>
+          </FilterItem>
+        </FilterList>
+      </Container>
+      {
+        currentPage >= totalPages ? (
+          <IconNext onClick={() => setCurrentPage(currentPage - 1)}>
+            <img src="/assets/icons/arrow-left.svg" alt="previus" />
+          </IconNext>
+        )
+          : (
+            <IconNext onClick={() => setCurrentPage(currentPage + 1)}>
+              <img src="/assets/icons/arrow-right.svg" alt="next" />
+            </IconNext>
+          )
+      }
+
+    </FilterContainer>
+  );
+}
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6.5rem 13.2rem 6.7rem;
+`;
+
+const Container = styled.div`
+  
+  display: flex;
+  gap: 2.4rem;
+`;
+
+const ProductsNumber = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 2.4rem;
+  color: #616161;
+`;
+
+const FilterTitle = styled.p`
+  display: flex;
+  align-items: center;
+  font-size: 2.4rem;
+  color: #a3a3a3;
+`;
+
+const FilterList = styled.ul`
+  display: flex;
+  gap: 2.4rem;
+`;
+
+const FilterItem = styled.li`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #ededed;
+  color: #a3a3a3;
+  border-radius: 10rem;
+  height: 4.8rem;
+  cursor: pointer;
+
+  &.active {
+    background: #0ad4fa;
+    
+    a{
+      color: #fff;
+    }
+
+  }
+`;
+
+const Button = styled.a`
+  font-size: 2.4rem;
+  color: #a3a3a3;
+  padding: 0 2.4rem;
+`;
+
+const IconNext = styled.div`
+  cursor: pointer;
+`;
+
+const Divider = styled.hr`
+  background: #d9d9d9;
+  opacity: .7;
+  width: .1rem;
+  height: 4.9rem;
+`;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,54 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+import Filter from '../components/Filter';
 import Hero from '../components/Hero';
 import ProductList from '../components/ProductList';
 
 export default function Home({ products, user }) {
+  const [filter, setFilter] = useState('Most recent');
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const sort = (sortProducts) => {
+    switch (filter) {
+      case 'Most recent':
+        return sortProducts.sort((a, b) => b._id.localeCompare(a._id));
+      case 'Lowest price':
+        return sortProducts.sort((a, b) => a.cost - b.cost);
+      case 'Highest price':
+        return sortProducts.sort((a, b) => b.cost - a.cost);
+      default:
+        return sortProducts;
+    }
+  };
+
+  const paginate = (sortProducts) => {
+    const pageSize = 16;
+    const pageNumber = currentPage;
+    return sortProducts.slice(pageNumber * pageSize, pageNumber * pageSize + pageSize);
+  };
+
   return (
-    <>
+    <ContainerHome>
       <Hero />
+      <Filter
+        filter={filter}
+        setFilter={setFilter}
+        count={products.length}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        pageSize={16}
+        totalPages={Math.ceil(products.length / 16) - 1}
+      />
       <Container>
-        <ProductList products={products} user={user} />
+        <ProductList products={paginate(sort(products))} user={user} />
       </Container>
-    </>
+    </ContainerHome>
   );
 }
+
+const ContainerHome = styled.div`
+  background:#f9f9f9;
+`;
 
 const Container = styled.div`
   padding: 4.2rem 13.2rem 6.0rem;
